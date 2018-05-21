07fab1633df67e1dbc4f74cb90b46bb66247d466
Alex Van Camp <vancamp.alex@gmail.com>
2018-05-20 21:51:57 -0500

    wip(transition): first anim works, but perf is a disaster

--- graphics/elements/molecules/gdq-transition/gdq-transition.js
+++ graphics/elements/molecules/gdq-transition/gdq-transition.js
@@ -34,30 +34,56 @@
 
 			tl.addLabel('frontRects', 0);
 			tl.addLabel('frontTraps', 0.1);
+			tl.addLabel('backRects', 0.1667);
+			tl.addLabel('backTraps', 0.2334);
 
 			// Front rects.
 			tl.to(this.$.bottomFrontRect, 0.2167, {
-				x: 21,
-				y: 256.6,
+				x: 26,
+				y: 321,
 				ease: 'ModifiedPower2EaseInOut'
 			}, 'frontRects');
 			tl.to(this.$.topFrontRect, 0.2167, {
-				x: -8,
-				y: -279,
+				x: -10,
+				y: -349,
 				ease: 'ModifiedPower2EaseInOut'
 			}, 'frontRects');
 
 			// Front traps.
 			tl.to(this.$.bottomFrontTrapezoid, 0.2667, {
-				x: -402,
-				y: 291.2,
+				x: -503,
+				y: 364,
 				ease: 'ModifiedPower2EaseInOut'
 			}, 'frontTraps');
 			tl.to(this.$.topFrontTrapezoid, 0.2667, {
-				x: 6,
-				y: -283.4,
+				x: 8,
+				y: -417,
 				ease: 'ModifiedPower2EaseInOut'
 			}, 'frontTraps');
+
+			// Back rects.
+			tl.to(this.$.bottomBackRect, 0.2334, {
+				x: -26,
+				y: 323,
+				ease: 'ModifiedPower2EaseInOut'
+			}, 'backRects');
+			tl.to(this.$.topBackRect, 0.2334, {
+				x: -10,
+				y: -351,
+				ease: 'ModifiedPower2EaseInOut'
+			}, 'backRects');
+
+			// Back traps.
+			tl.to(this.$.bottomBackTrapezoid, 0.2334, {
+				x: -490,
+				y: 374,
+				ease: 'ModifiedPower2EaseInOut'
+			}, 'backTraps');
+			tl.to(this.$.topBackTrapezoid, 0.2334, {
+				x: 0,
+				y: -426,
+				ease: 'ModifiedPower2EaseInOut'
+			}, 'backTraps');
 
 			return tl;
 		}
