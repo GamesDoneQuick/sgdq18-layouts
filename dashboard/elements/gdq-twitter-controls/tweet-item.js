1131ef97e42655a53915d84d1169a5e08ebff65e
Alex Van Camp <vancamp.alex@gmail.com>
2018-05-28 11:53:27 -0500

    fix: add mutabledata mixin to tweet-item

--- dashboard/elements/gdq-twitter-controls/tweet-item.js
+++ dashboard/elements/gdq-twitter-controls/tweet-item.js
@@ -1,52 +1,61 @@
-class TweetItem extends Polymer.Element {
-	static get is() {
-		return 'tweet-item';
+(function () {
+	'use strict';
+
+	/**
+	 * @customElement
+	 * @polymer
+	 * @appliesMixin Polymer.MutableData
+	 */
+	class TweetItem extends Polymer.MutableData(Polymer.Element) {
+		static get is() {
+			return 'tweet-item';
+		}
+
+		static get properties() {
+			return {
+				value: {
+					type: Object,
+					observer: '_valueChanged'
+				},
+				profileUrl: {
+					type: String,
+					computed: 'computeProfileUrl(value)'
+				},
+				tweetUrl: {
+					type: String,
+					computed: 'computeTweetUrl(profileUrl, value)'
+				}
+			};
+		}
+
+		_valueChanged(newValue) {
+			Polymer.dom(this.$.body).innerHTML = newValue.text;
+		}
+
+		computeProfileUrl(value) {
+			return `https://twitter.com/${value.user.screen_name}`;
+		}
+
+		computeTweetUrl(profileUrl, value) {
+			return `${profileUrl}/status/${value.id_str}`;
+		}
+
+		computePhotoOrPhotos(numPhotos) {
+			return numPhotos > 1 ? 'photos' : 'photo';
+		}
+
+		computeIndexPlusOne(index) {
+			return index + 1;
+		}
+
+		accept() {
+			nodecg.sendMessage('acceptTweet', this.value);
+		}
+
+		reject() {
+			nodecg.sendMessage('rejectTweet', this.value.id_str);
+		}
 	}
 
-	static get properties() {
-		return {
-			value: {
-				type: Object,
-				observer: '_valueChanged'
-			},
-			profileUrl: {
-				type: String,
-				computed: 'computeProfileUrl(value)'
-			},
-			tweetUrl: {
-				type: String,
-				computed: 'computeTweetUrl(profileUrl, value)'
-			}
-		};
-	}
-
-	_valueChanged(newValue) {
-		Polymer.dom(this.$.body).innerHTML = newValue.text;
-	}
-
-	computeProfileUrl(value) {
-		return `https://twitter.com/${value.user.screen_name}`;
-	}
-
-	computeTweetUrl(profileUrl, value) {
-		return `${profileUrl}/status/${value.id_str}`;
-	}
-
-	computePhotoOrPhotos(numPhotos) {
-		return numPhotos > 1 ? 'photos' : 'photo';
-	}
-
-	computeIndexPlusOne(index) {
-		return index + 1;
-	}
-
-	accept() {
-		nodecg.sendMessage('acceptTweet', this.value);
-	}
-
-	reject() {
-		nodecg.sendMessage('rejectTweet', this.value.id_str);
-	}
-}
-
-customElements.define(TweetItem.is, TweetItem);
+	customElements.define(TweetItem.is, TweetItem);
+})();
