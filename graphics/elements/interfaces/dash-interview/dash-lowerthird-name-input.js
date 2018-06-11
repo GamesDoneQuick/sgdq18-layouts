705f52a91e76d45dee618934217be7a89b8b2402
Alex Van Camp <vancamp.alex@gmail.com>
2018-06-11 18:54:46 -0500

    feat(lowerthird): add optional "Title" field & graphic to all lowerthird nameplates (#8)

--- graphics/elements/interfaces/dash-interview/dash-lowerthird-name-input.js
+++ graphics/elements/interfaces/dash-interview/dash-lowerthird-name-input.js
@@ -9,7 +9,11 @@ class DashLowerthirdNameInput extends Polymer.Element {
 				type: String,
 				notify: true
 			},
-			value: {
+			name: {
+				type: String,
+				notify: true
+			},
+			title: {
 				type: String,
 				notify: true
 			},
@@ -20,16 +24,17 @@ class DashLowerthirdNameInput extends Polymer.Element {
 
 	ready() {
 		super.ready();
-		this.$.input.$.input.style.fontSize = '24px';
-		this.$.input.$.input.style.height = '45px';
-		this.$.input.$.toggleIcon.style.height = '100%';
-		this.$.input.$.toggleIcon.style.padding = '0';
-		this.$.input.$.clearIcon.style.height = '100%';
-		this.$.input.$.clearIcon.style.padding = '0';
+		this.$.name.$.input.style.fontSize = '24px';
+		this.$.name.$.input.style.height = '45px';
+		this.$.name.$.toggleIcon.style.height = '100%';
+		this.$.name.$.toggleIcon.style.padding = '0';
+		this.$.name.$.clearIcon.style.height = '100%';
+		this.$.name.$.clearIcon.style.padding = '0';
 	}
 
 	clear() {
-		this.$.input.value = '';
+		this.$.name.value = '';
+		this.$.name.value = '';
 	}
 }
 
