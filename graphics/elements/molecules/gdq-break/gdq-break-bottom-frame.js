aa2d6a1870f68ffff65bc5ac5642e0a6ee71ab22
Alex Van Camp <vancamp.alex@gmail.com>
2018-06-18 19:19:12 -0500

    wip(sub-alerts): implement sub alerts on the break screen

--- graphics/elements/molecules/gdq-break/gdq-break-bottom-frame.js
+++ graphics/elements/molecules/gdq-break/gdq-break-bottom-frame.js
@@ -39,6 +39,8 @@
 				this._changeText(this.$['music-text'], `${newVal.game || '?'} - ${newVal.title || '?'}`);
 			});
 
+			nodecg.listenFor('subscription', this._handleSubscription.bind(this));
+
 			// Logo anim
 			const logoTL = new TimelineMax({repeat: -1});
 
@@ -76,6 +78,40 @@
 						delay: 0.05
 					});
 				}
+			});
+		}
+
+		_handleSubscription(subscription) {
+			let backgroundColor = 'white';
+			let holdDuration = 0.067;
+			let text = 'New';
+
+			if (subscription.sub_plan && subscription.sub_plan.toLowerCase() === 'prime') {
+				backgroundColor = '#6441a4';
+				text = 'Prime';
+			} else if (subscription.context && subscription.context.toLowerCase() === 'subgift') {
+				backgroundColor = '#00ffff';
+				text = 'Gift';
+			} else if (subscription.sub_plan === '2000') {
+				backgroundColor = '#ffba00';
+				holdDuration *= 3;
+				text = '$9.99';
+			} else if (subscription.sub_plan === '3000') {
+				backgroundColor = '#ff0099';
+				holdDuration *= 6;
+				text = '$24.99';
+			}
+
+			if (subscription.months <= 1) {
+				text += ' Sub';
+			} else {
+				text += ` Resub x${subscription.months}`;
+			}
+
+			this.$.subscriptionAlerts.addAlert({
+				text,
+				backgroundColor,
+				holdDuration
 			});
 		}
 	}
