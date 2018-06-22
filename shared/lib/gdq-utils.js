3fb415bfd523b4cfc7f2cea20818c98ca3f5c9f4
Alex Van Camp <vancamp.alex@gmail.com>
2018-06-22 17:52:22 -0500

    fix(gdqUtils): fix name of browser export

--- shared/lib/gdq-utils.js
+++ shared/lib/gdq-utils.js
@@ -7,7 +7,7 @@
 		module.exports = factory();
 	} else {
 		// Browser globals (root is window)
-		root.returnExports = factory();
+		root.gdqUtils = factory();
 	}
 }(typeof self === 'undefined' ? this : self, () => {
 	'use strict';
